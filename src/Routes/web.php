9344266c894a801b9aa280ff28044b6ca94abfec
maxboyko <maxboiko@divotek.com>
2025-03-17 16:00:50 +0200

    add compare count api

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use SmartCms\Compare\Routes\CompareHandler;
 use SmartCms\Compare\Services\CompareService;
+use SmartCms\Core\Services\ScmsResponse;
 
 Route::get('/api/compare', CompareHandler::class)->middleware(['lang', 'web'])->name('compare.index');
+Route::get('/api/compare/count', function () {
+    return new ScmsResponse(200, [
+        'count' => CompareService::getProductsCount(),
+    ]);
+})->middleware(['lang', 'web'])->name('compare.index');
